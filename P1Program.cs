7e65c62e34fe6b325ad3a65ffec539b79436eb45
jsolanoo <n00443270@upn.pe>
2024-09-19 09:31:34 -0500

    Primer commit

---
+++ P1Program.cs
@@ -0,0 +1,43 @@
+﻿double salarioBruto, ventas, comision, totalg, descuento, salarioNeto;
+int anosemp;
+Console.Write("Ingrese su nombre: ");
+string nombre = Console.ReadLine();
+Console.Write("Ingrese su salario bruto del mes: ");
+salarioBruto=double.Parse(Console.ReadLine());
+Console.Write("Ingrese las ventas realizadas: ");
+ventas=double.Parse(Console.ReadLine());
+Console.Write("Ingrese la cantidad de años en la empresa: ");
+anosemp=int.Parse(Console.ReadLine());
+//PROCESO
+
+{
+    if (ventas <= 600)
+    {
+        comision = ventas * 0.05;
+    }
+    else if (ventas > 600 && ventas <= 3000)
+    {
+        comision = ventas * 0.07;
+    }
+    else
+    {
+        comision = 300 + ventas * 0.06;
+    }
+    totalg = salarioBruto + comision;
+}
+if(anosemp<=3)
+{
+    descuento = 0.065 * totalg;
+}
+else
+{
+    descuento = 0.045 * totalg;
+}
+
+salarioNeto = totalg - descuento;
+//RESULT
+Console.WriteLine("Nombre: "+nombre);
+Console.WriteLine("Total ganado: S/. "+totalg);
+Console.WriteLine("Descuento en dinero: S/. "+descuento);
+Console.WriteLine("Salario neto que recibirá: S/. "+salarioNeto);
+Console.ReadKey();
---
+++ P2Program.cs
@@ -0,0 +1,41 @@
+﻿int contadorVerde = 0;
+int contadorAzul = 0;
+int contadorRojo = 0;
+    while (true)
+    {
+        Console.Write("Ingrese el último dígito de la placa del automóvil (o 'F' para finalizar): ");
+        string entrada = Console.ReadLine();
+        if (entrada.ToUpper() == "F")
+        {
+            break;
+        }
+        if (int.TryParse(entrada, out int digito))
+        {
+            if (digito >= 0 && digito <= 3)
+            {
+                contadorVerde++;
+            }
+            else if (digito >= 4 && digito <= 6)
+            {
+                contadorAzul++;
+            }
+            else if (digito >= 7 && digito <= 9)
+            {
+                contadorRojo++;
+            }
+            else
+            {
+                Console.WriteLine("El dígito debe estar entre 0 y 9.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Entrada no válida. Debe ser un dígito o 'F' para finalizar.");
+        }
+    }
+Console.WriteLine($"Autos con calcomanía verde: {contadorVerde}");
+Console.WriteLine($"Autos con calcomanía azul: {contadorAzul}");
+Console.WriteLine($"Autos con calcomanía roja: {contadorRojo}");
+Console.ReadKey();
+
+
